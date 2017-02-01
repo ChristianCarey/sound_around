558e0848728a2b287ff49c995e726a79524045aa
Christian Carey <christianrcarey@gmail.com>
2017-02-01 10:59:37 -0500

    filter by sold out out events

--- app/assets/javascripts/ng/controllers/artist-show.controller.js
+++ app/assets/javascripts/ng/controllers/artist-show.controller.js
@@ -1,7 +1,9 @@
 soundAround.controller('ArtistShowCtrl', ['$scope', '$stateParams', 'artist',
   function($scope, $stateParams, artist) {
-    $scope.artist = artist
-    $scope.eventOrder = 'datetime'
+    var allEvents = angular.copy(artist.events);
+    $scope.artist = artist;
+    $scope.eventOrder = 'datetime';
+    $scope.soldOutShowing = true;
 
     $scope.setEventOrder = function(order) {
       $scope.eventOrder = order;
@@ -10,5 +12,21 @@ soundAround.controller('ArtistShowCtrl', ['$scope', '$stateParams', 'artist',
       } else {
         $scope.eventOrderString = 'venue.miles_away'
       }
+    }
+
+    $scope.showSoldOut = function() {
+      $scope.soldOutShowing = true;
+      angular.copy(allEvents, $scope.artist.events)
+    }
+
+    $scope.hideSoldOut = function() {
+      $scope.soldOutShowing = false;
+      var notSoldOut = []
+      artist.events.forEach(function(event) {
+        if (event.ticket_status === 'available') {
+          notSoldOut.push(event);
+        }
+      })
+      angular.copy(notSoldOut, $scope.artist.events)
     }
   }]);
